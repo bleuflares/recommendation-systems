ad4a96981dbbed8ff42d2bb14070db20c19ce3ad
박기성 <bleuflares@gmail.com>
2018-10-31 21:31:38 +0900

    fixed error in trainset divider

--- train_set_builder.py
+++ train_set_builder.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 	for j in range(18):
 		output_file = open("train_%d.txt" %j, 'w')
 		for i in range(len(lines)):
-			if 5000 * j > i or i <= 5000 * (j + 1):
+			if 5000 * j > i or i >= 5000 * (j + 1):
 				output_file.write(lines[i])
 		output_file.close()
 		
