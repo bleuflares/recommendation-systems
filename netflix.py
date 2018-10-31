46491645af97d6466460287653c08562d2863418
박기성 <bleuflares@gmail.com>
2018-11-01 02:43:58 +0900

    1.25 still big final trial of 1.15

--- netflix.py
+++ netflix.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
     #print(mat)
 
     output_file = open(sys.argv[2], 'r')
-    weight = 1.25
+    weight = 1.15
     rmse = 0.0
     count = 0
     for line in output_file:
